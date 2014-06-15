efb63c19200c4a2bcf29376b9c438c560794936b
usatie <SHUN@Shun-no-MacBook-Air.local>
2014-06-15 18:06:14 +0900

    Initial Commit

---
+++ gitTest1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitTest1
+//
+//  Created by Shun Usami on 2014/06/15.
+//  Copyright (c) 2014年 ShunUsami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ gitTest1Tests/gitTest1Tests.m
@@ -0,0 +1,34 @@
+//
+//  gitTest1Tests.m
+//  gitTest1Tests
+//
+//  Created by Shun Usami on 2014/06/15.
+//  Copyright (c) 2014年 ShunUsami. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gitTest1Tests : XCTestCase
+
+@end
+
+@implementation gitTest1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
